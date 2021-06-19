9c5c87df86d0aa66dc1c841acd001133f4955bbd
Fernandes <vs.masich@gmail.com>
2021-06-19 11:20:51 +0300

    adding less

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,14 @@
+const path = require('path');
 const router = require('express').Router();
 const products = require('./products');
 const categories = require('./categories');
 const NotFoundError = require('../errors/NotFoundError');
+const publicPath = path.join(__dirname, '../..', 'build');
 
 router.use('/products', products);
 router.use('/categories', categories);
 router.use('/*', (req, res, next) => {
-  next(new NotFoundError('Страница не найдена'));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 module.exports = router;
