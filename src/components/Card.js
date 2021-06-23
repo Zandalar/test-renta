66ed42bf70eb37473c114120714612c2048f540c
Zandalar <vs.masich@gmail.com>
2021-06-23 17:11:22 +0300

    feat: add basket sum redux, fix cards counter

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import burger from '../images/burger.png';
 import { useDispatch, useSelector } from 'react-redux';
 import {setOrderCounter} from '../reducers/orderCounter';
@@ -7,7 +7,9 @@ import {setOrderCounter} from '../reducers/orderCounter';
 const Card = ({ data, sectionType }) => {
   const [isCardHover, setIsCardHover] = useState(false);
   const [isOrderCounterShown, setIsOrderCounterShown] = useState(false);
+  const [productCount, setProductCount] = useState(1);
   const counter = useSelector((state) => state.orderCounter.orderCounter);
+  const isDelivery = useSelector((state) => state.delivery.isDelivery);
   const dispatch = useDispatch();
 
   function onHoverCard() {
@@ -20,22 +22,36 @@ const Card = ({ data, sectionType }) => {
 
   function onCounterClick() {
     setIsOrderCounterShown(true);
-    dispatch(setOrderCounter(counter + 1));
+    dispatch(setOrderCounter(counter + data.price));
   }
 
   function increaseCounter() {
-    dispatch(setOrderCounter(counter + 1));
+    setProductCount(productCount + 1);
+    dispatch(setOrderCounter(counter + data.price));
   }
 
   function decreaseCounter() {
-    if (counter < 2) {
+    setProductCount(productCount - 1);
+    dispatch(setOrderCounter(counter - data.price));
+    if (productCount < 2) {
       setIsOrderCounterShown(false);
+      setProductCount(1);
     }
-    dispatch(setOrderCounter(counter - 1));
   }
 
+  useEffect(() => {
+    setProductCount(1);
+    setIsOrderCounterShown(false);
+    dispatch(setOrderCounter(0));
+  }, [isDelivery])
+
   return (
-    <li className={`card${isCardHover ? ((sectionType === 'section') ? ' card_active-grey' : ' card_active') : ''}`} id={data._id} onMouseOver={onHoverCard} onMouseLeave={onLeaveCard}>
+    <li
+      className={`card${isCardHover ? ((sectionType === 'section') ? ' card_active-grey' : ' card_active') : ''}`}
+      id={data._id}
+      onMouseOver={onHoverCard}
+      onMouseLeave={onLeaveCard}
+    >
       <img className='card__image' src={burger} alt={data.name} />
       <p className='card__name'>{data.name}</p>
       <p className='card__price'>{data.price} &#8381;</p>
@@ -44,7 +60,7 @@ const Card = ({ data, sectionType }) => {
       {isOrderCounterShown
         ? <div className='card__counter'>
           <button className='card__btn card__btn_minus' type='button' onClick={decreaseCounter} />
-          <p className='card__counter-value'>{counter}</p>
+          <p className='card__counter-value'>{productCount}</p>
           <button className='card__btn card__btn_plus' type='button' onClick={increaseCounter} />
         </div>
         : <button className='card__btn' type='button' onClick={onCounterClick} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import logo from '../images/logo.svg';
 import emptyBasket from '../images/icon__basket.svg';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const counter = useSelector((state) => state.orderCounter.orderCounter);
+
   return (
     <header className='header'>
       <div className='header__container'>
         <img className='header__logo' src={logo} alt='logo' />
         <button className='header__menu' type='button' />
         <div className='header__basket'>
-          <p className='header__basket-count'>144 &#8381;</p>
+          <p className='header__basket-count'>{counter} &#8381;</p>
           <img className='header__basket-image' src={emptyBasket} alt='empty basket' />
         </div>
       </div>
